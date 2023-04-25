c720dca84cf009b221fc622564ecd60198d28373
Guthi Gowtham Veerabadra Swamy <guthi.gowtham.veerabadra.swamy@ibm.com>
2023-04-25 12:50:48 +0530

    added rest controller for sample hello world api

---
+++ src/main/java/com/example/GradleDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RESTController;
+
+@RestController
+@SpringBootApplication
+public class GradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
